79ef2862763ec1203fd76ad470b4b749c0cac99a
RicoPriyono <Ricopriyono21@gmail.com>
2022-06-26 03:03:31 -0700

    Revert "PilihanJual"
    This reverts commit 5b20bc9458672447d772416a4e8a6588c01ae183.

--- lib/ui_view/pilihanpenjual.dart
+++ lib/ui_view/pilihanpenjual.dart
@@ -16,22 +16,22 @@ class Pilihan_Jual extends StatefulWidget {
   @override
   widget build(BuildContext context) {
     return Scaffold(
-      appBar: new AppBar(
-        title: Text(""),
-        backgroundColor: Colors.grey[700],
-      ),
-      backgroundColor: Colors.grey[100],
-      body: Container(
-        padding: EdgeInsets.all(30.0),
-        child: GridView.count(
-            crossAxisCount: 2,
-            children: <Widget>[
-        Card(
-        margin: EdgeInsets.all(8.0),
-        child: InkWell(
-          onTap: (){},
-          splashcolor: Colors.grey,
-          child: Center(
+        appBar: new AppBar(
+          title: Text(""),
+          backgroundColor: Colors.grey[700],
+        ),
+        backgroundColor: Colors.grey[100],
+        body: Container(
+            padding: EdgeInsets.all(30.0),
+            child: GridView.count(
+                crossAxisCount: 2,
+                children: <Widget>[
+            Card(
+            margin: EdgeInsets.all(8.0),
+            child: InkWell(
+            onTap: (){},
+        splashcolor: Colors.grey,
+        child: Center(
             child: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: <widget>[
@@ -40,22 +40,22 @@ class Pilihan_Jual extends StatefulWidget {
             height: 55,
             width: 120,
             decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage("assets/images/motornew.png"))),
-          ),
-          Container(
-            margin: EdgeInsets.only(top: 25, left: 200),
-            height: 55,
-            width: 120,
-            decoration: BoxDecoration(
-                image: DecorationImage(
-                    image:
-                    AssetImage("assets/images/partnew.png"))),
-            ],
-          ),
+              image: DecorationImage(
+                  image: AssetImage("assets/images/motornew.png"))),
+        ),
+            Container(
+                margin: EdgeInsets.only(top: 25, left: 200),
+                height: 55,
+                width: 120,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image:
+                        AssetImage("assets/images/partnew.png"))),
+                ],
+            ),
         ),
       ),
-    ),
+     ),
     ],
     ),
     ),
